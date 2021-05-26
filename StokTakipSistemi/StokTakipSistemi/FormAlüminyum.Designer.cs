fb6b4b8b464819ec049a2b5870e30c11d09b7628
Furkan Güzelgörür <furkanguzelgorur@gmail.com>
2021-05-26 18:04:56 +0300

    sql bağlantısı yapıldı
    sql bağlantısı yapıldı

--- StokTakipSistemi/StokTakipSistemi/FormAlüminyum.Designer.cs
+++ StokTakipSistemi/StokTakipSistemi/FormAlüminyum.Designer.cs
@@ -30,110 +30,93 @@ namespace StokTakipSistemi
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnCikar = new System.Windows.Forms.Button();
+            this.btnEkle = new System.Windows.Forms.Button();
             this.btnAlmListele = new System.Windows.Forms.Button();
-            this.listView1 = new System.Windows.Forms.ListView();
+            this.comboxalümin = new System.Windows.Forms.ComboBox();
+            this.txtboxMiktar = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lstviewAlümin = new System.Windows.Forms.ListView();
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.button1 = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.button1);
-            this.groupBox1.Controls.Add(this.comboBox1);
-            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.btnCikar);
+            this.groupBox1.Controls.Add(this.btnEkle);
+            this.groupBox1.Controls.Add(this.btnAlmListele);
+            this.groupBox1.Controls.Add(this.comboxalümin);
+            this.groupBox1.Controls.Add(this.txtboxMiktar);
             this.groupBox1.Controls.Add(this.label5);
             this.groupBox1.Controls.Add(this.label6);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Location = new System.Drawing.Point(12, 23);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(346, 256);
+            this.groupBox1.Size = new System.Drawing.Size(609, 147);
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Yeni Alüminyum Ekle";
+            this.groupBox1.Text = "Alüminyum Ekle";
+            // 
+            // btnCikar
+            // 
+            this.btnCikar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnCikar.Location = new System.Drawing.Point(185, 80);
+            this.btnCikar.Name = "btnCikar";
+            this.btnCikar.Size = new System.Drawing.Size(106, 39);
+            this.btnCikar.TabIndex = 15;
+            this.btnCikar.Text = "ÇIKAR";
+            this.btnCikar.UseVisualStyleBackColor = true;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEkle.Location = new System.Drawing.Point(47, 80);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(106, 39);
+            this.btnEkle.TabIndex = 14;
+            this.btnEkle.Text = "EKLE";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.button1_Click);
             // 
             // btnAlmListele
             // 
             this.btnAlmListele.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.btnAlmListele.Location = new System.Drawing.Point(392, 12);
+            this.btnAlmListele.Location = new System.Drawing.Point(324, 80);
             this.btnAlmListele.Name = "btnAlmListele";
-            this.btnAlmListele.Size = new System.Drawing.Size(142, 44);
+            this.btnAlmListele.Size = new System.Drawing.Size(174, 40);
             this.btnAlmListele.TabIndex = 8;
             this.btnAlmListele.Text = "LİSTELE";
             this.btnAlmListele.UseVisualStyleBackColor = true;
+            this.btnAlmListele.Click += new System.EventHandler(this.btnAlmListele_Click);
             // 
-            // listView1
+            // comboxalümin
             // 
-            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.columnHeader1,
-            this.columnHeader2,
-            this.columnHeader3,
-            this.columnHeader4});
-            this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(392, 64);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(375, 334);
-            this.listView1.TabIndex = 11;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.View = System.Windows.Forms.View.Details;
+            this.comboxalümin.FormattingEnabled = true;
+            this.comboxalümin.Location = new System.Drawing.Point(141, 37);
+            this.comboxalümin.Name = "comboxalümin";
+            this.comboxalümin.Size = new System.Drawing.Size(194, 24);
+            this.comboxalümin.TabIndex = 13;
             // 
-            // columnHeader1
+            // txtboxMiktar
             // 
-            this.columnHeader1.Text = "Ürün Adı";
-            this.columnHeader1.Width = 105;
-            // 
-            // columnHeader2
-            // 
-            this.columnHeader2.Text = "Miktar";
-            // 
-            // columnHeader3
-            // 
-            this.columnHeader3.Text = "Çap";
-            this.columnHeader3.Width = 87;
-            // 
-            // columnHeader4
-            // 
-            this.columnHeader4.Text = "Ağırlık";
-            this.columnHeader4.Width = 62;
-            // 
-            // button1
-            // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.button1.Location = new System.Drawing.Point(154, 133);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(106, 39);
-            this.button1.TabIndex = 14;
-            this.button1.Text = "EKLE";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(154, 52);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(170, 24);
-            this.comboBox1.TabIndex = 13;
-            // 
-            // textBox5
-            // 
-            this.textBox5.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.textBox5.Location = new System.Drawing.Point(154, 101);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(170, 26);
-            this.textBox5.TabIndex = 12;
+            this.txtboxMiktar.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtboxMiktar.Location = new System.Drawing.Point(456, 37);
+            this.txtboxMiktar.Name = "txtboxMiktar";
+            this.txtboxMiktar.Size = new System.Drawing.Size(121, 26);
+            this.txtboxMiktar.TabIndex = 12;
+            this.txtboxMiktar.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label5.Location = new System.Drawing.Point(40, 52);
+            this.label5.Location = new System.Drawing.Point(33, 37);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(102, 24);
             this.label5.TabIndex = 10;
@@ -143,22 +126,67 @@ namespace StokTakipSistemi
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label6.Location = new System.Drawing.Point(12, 100);
+            this.label6.Location = new System.Drawing.Point(367, 36);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(130, 24);
+            this.label6.Size = new System.Drawing.Size(83, 24);
             this.label6.TabIndex = 11;
-            this.label6.Text = "Miktar (Kg) :";
+            this.label6.Text = "Miktar :";
+            // 
+            // lstviewAlümin
+            // 
+            this.lstviewAlümin.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.lstviewAlümin.HideSelection = false;
+            this.lstviewAlümin.Location = new System.Drawing.Point(12, 176);
+            this.lstviewAlümin.Name = "lstviewAlümin";
+            this.lstviewAlümin.Size = new System.Drawing.Size(609, 273);
+            this.lstviewAlümin.TabIndex = 11;
+            this.lstviewAlümin.UseCompatibleStateImageBehavior = false;
+            this.lstviewAlümin.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Ürün Adı";
+            this.columnHeader1.Width = 170;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Çap";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader2.Width = 114;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Kalınlık";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader3.Width = 77;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Ağırlık";
+            this.columnHeader4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader4.Width = 111;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Miktar";
+            this.columnHeader5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader5.Width = 123;
             // 
             // FormAlüminyum
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.lstviewAlümin);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.btnAlmListele);
             this.Name = "FormAlüminyum";
             this.Text = "ALÜMİNYUM GİRİŞ";
+            this.Load += new System.EventHandler(this.FormAlüminyum_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
@@ -168,15 +196,17 @@ namespace StokTakipSistemi
         #endregion
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button btnAlmListele;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.ComboBox comboxalümin;
+        private System.Windows.Forms.TextBox txtboxMiktar;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView lstviewAlümin;
         private System.Windows.Forms.ColumnHeader columnHeader1;
         private System.Windows.Forms.ColumnHeader columnHeader2;
         private System.Windows.Forms.ColumnHeader columnHeader3;
         private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.Button btnCikar;
     }
 }
--- StokTakipSistemi/StokTakipSistemi/FormAlüminyum.cs
+++ StokTakipSistemi/StokTakipSistemi/FormAlüminyum.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace StokTakipSistemi
 {
@@ -15,6 +16,64 @@ namespace StokTakipSistemi
         public FormAlüminyum()
         {
             InitializeComponent();
+        }
+        SqlConnection baglanti = new SqlConnection("Data Source=DESKTOP-BGJRLTE\\SQLEXPRESS;Initial Catalog=StokTakipSistemi;Integrated Security=True");
+
+        private void verilerigörüntüle()
+        {
+            lstviewAlümin.Items.Clear();
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("Select *From alüminyumstok", baglanti);
+            SqlDataReader oku = komut.ExecuteReader();
+
+            while (oku.Read())
+            {
+                ListViewItem ekle = new ListViewItem();
+                ekle.Text = oku["alüminyumadi"].ToString();
+                ekle.SubItems.Add(oku["alüminyumcap"].ToString());
+                ekle.SubItems.Add(oku["alüminyumkalinlik"].ToString());
+                ekle.SubItems.Add(oku["alüminyumagirlik"].ToString());
+                ekle.SubItems.Add(oku["alüminyumstok"].ToString());
+
+                lstviewAlümin.Items.Add(ekle);
+            }
+            baglanti.Close();
+        }
+
+
+        private void FormAlüminyum_Load(object sender, EventArgs e)
+        {
+            SqlConnection baglanti = new SqlConnection();
+            baglanti.ConnectionString = "Data Source=DESKTOP-BGJRLTE\\SQLEXPRESS;Initial Catalog=StokTakipSistemi;Integrated Security=SSPI";
+            SqlCommand komut = new SqlCommand();
+            komut.CommandText = "SELECT *FROM alüminyumstok";
+            komut.Connection = baglanti;
+            komut.CommandType = CommandType.Text;
+
+            SqlDataReader dr;
+            baglanti.Open();
+            dr = komut.ExecuteReader();
+            while (dr.Read())
+            {
+                comboxalümin.Items.Add(dr["alüminyumadi"]);
+            }
+            baglanti.Close();
+
+        }
+
+        private void btnAlmListele_Click(object sender, EventArgs e)
+        {
+            verilerigörüntüle();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
